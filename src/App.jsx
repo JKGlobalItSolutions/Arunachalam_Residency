b2434bba5327b43576d5eaf03215e54bdf7a7ac1
sriram A <sriram.coder2004@gmail.com>
2025-06-07 12:14:06 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
-import { HashRouter as Router, Routes, Route } from "react-router-dom";
-
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import './App.css';
 
 import Navbar from "./components/Navbar";
@@ -11,23 +10,32 @@ import Aminities from "./pages/Amenities";
 import Gallery from "./pages/Gallery";
 import Contact from "./pages/Contact";
 
-
-
-
 function App() {
   return (
     <Router>
       <Navbar />
-     
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/rooms" element={<Rooms />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/amenities" element={<Aminities />} />
-          <Route path="/gallery" element={<Gallery />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
-   
+
+      {/* Navigation Menu (can be inside Navbar too) */}
+      <nav>
+        <ul>
+          <li><Link to="/">Home</Link></li>
+          <li><Link to="/rooms">Rooms</Link></li>
+          <li><Link to="/about">About</Link></li>
+          <li><Link to="/amenities">Amenities</Link></li>
+          <li><Link to="/gallery">Gallery</Link></li>
+          <li><Link to="/contact">Contact</Link></li>
+        </ul>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/rooms" element={<Rooms />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/amenities" element={<Aminities />} />
+        <Route path="/gallery" element={<Gallery />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+
       <Footer />
     </Router>
   );
